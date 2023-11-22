0b03ed0d7b21b4c4e5754be5ce2352709952076b
Andrew Ketsko <monicman82@gmail.com>
2023-11-22 14:58:35 +0200

    user prepare done

---
+++ src/users/user.entity.ts
@@ -0,0 +1,34 @@
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Insert User with id ', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Update User with id ', this.id);
+  }
+  @AfterRemove()
+  logRemove() {
+    console.log('Remove User with id ', this.id);
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    return this.userService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id', ParseIntPipe) id: number) {
+    const user = await this.userService.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateUserDto,
+  ) {
+    return this.userService.update(id, body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.remove(id);
+  }
+}
